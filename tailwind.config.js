8e37e5c16b7d2cf7d36454e8abab308f313c79ab
venkat <techvenkats@gmail.com>
2024-12-12 12:51:24 +0530

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       animation: {
