8a691a60fa3e16f626cc6cc54fd13261c258fa98
venkat <techvenkats@gmail.com>
2024-12-12 13:33:12 +0530

    Removed unwanted files

--- src/components/common/BackgroundAnimation.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export const BackgroundAnimation = () => {
-    return (React.createElement("div", { className: "relative w-full h-full overflow-hidden" },
-        React.createElement("div", { className: "absolute top-[-100px] left-1/2 transform -translate-x-1/2 w-[400px] h-[200px] rounded-full bg-green-400 opacity-40 blur-3xl animate-glow" }),
-        React.createElement("div", { className: "absolute bottom-[-100px] left-1/2 transform -translate-x-1/2 w-[400px] h-[200px] rounded-full bg-green-400 opacity-40 blur-3xl animate-glow" })));
-};
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import React from 'react';
-createRoot(document.getElementById('root')).render(React.createElement(StrictMode, null,
-    React.createElement(App, null)));
